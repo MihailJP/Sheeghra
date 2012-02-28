c569d5e5c8c22eb84fd608d04729876042b5089a
TANIGAWA Takashi <mihailjp@gmail.com>
2012-02-28 16:00:21 +0900

    Add target to convert to SFD

---
+++ tools/conv2sfd.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/fontforge
+
+import sys
+import re
+import fontforge
+
+if (len(sys.argv) != 3):
+	print 'Usage: %s source-ttf vtp-file' % sys.argv[0]
+	exit(1)
+
+font = fontforge.open(sys.argv[1], 1)
+font.encoding = "original"
+
+vtp = open(sys.argv[2])
+reGlyph = re.compile("^DEF_GLYPH \"(\\w+)\" ID (\\d+)")
+for line in vtp.readlines():
+	m = reGlyph.match(line)
+	if m: font[int(m.group(2))].glyphname = m.group(1)
+
+font.save("/dev/stdout")
