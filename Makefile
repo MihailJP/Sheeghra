76c436fa3bb105d05796706b206a4aa2471a9b7a
TANIGAWA Takashi <mihailjp@gmail.com>
2012-02-29 02:38:16 +0900

    Update Makefile

--- Makefile
+++ Makefile
@@ -1,14 +1,21 @@
-.PHONY:fetch conv2sfd clean
+.PHONY: all fetch conv2sfd clean distclean
 DIRS=devanagari bengali gurmukhi gujarati oriya tamil telugu kannada malayalam
 FETCHDOCS=COPYING.txt
 
-fetch:
-	wget http://www.cdacmumbai.in/projects/indix/RaghuFonts/$(FETCHDOCS)
+all: conv2sfd
+
+fetch: COPYING.txt
 	for i in $(DIRS);do cd $$i; make $@; cd ..; done
 
-conv2sfd:
+COPYING.txt:
+	wget http://www.cdacmumbai.in/projects/indix/RaghuFonts/$(FETCHDOCS)
+
+conv2sfd: fetch
 	for i in $(DIRS);do cd $$i; make $@; cd ..; done
 
 clean:
+	for i in $(DIRS);do cd $$i; make $@; cd ..; done
+
+distclean: clean
 	-rm $(FETCHDOCS)
 	for i in $(DIRS);do cd $$i; make $@; cd ..; done
--- common/common.mk
+++ common/common.mk
@@ -1,19 +1,26 @@
-.PHONY: fetch conv2sfd clean
+.PHONY: all fetch conv2sfd clean distclean
 .SUFFIXES: .ttf .vps .sfd
 
-fetch: $(SRCTTF) $(SRCDOC)
+all: conv2sfd
+
+fetch: $(SRCTTF) $(BASETTF) $(SRCDOC)
 $(SRCTTF):
 	wget http://www.cdacmumbai.in/projects/indix/RaghuFonts/$(SRCTTF)
 $(SRCDOC):
 	wget http://www.cdacmumbai.in/projects/indix/RaghuFonts/$(SRCDOC)
+$(BASETTF):
+	wget http://www.cdacmumbai.in/projects/indix/RaghuFonts/$(BASETTF)
 
-conv2sfd: $(SRCTTF:.ttf=.sfd)
+conv2sfd: fetch $(SRCTTF:.ttf=.sfd)
 $(SRCTTF:.ttf=.vtp): $(SRCTTF)
 	../tools/extractvtp.pl $< > $@
-$(SRCTTF:.ttf=.sfd): $(SRCTTF) $(SRCTTF:.ttf=.vtp) $(SEDSCR)
+$(SRCTTF:.ttf=.sfd): $(BASETTF) $(SRCTTF:.ttf=.vtp) $(SEDSCR)
 	LANG=C;../tools/conv2sfd.py $(SRCTTF) $(SRCTTF:.ttf=.vtp) tmp.sfd
 	cat tmp.sfd | ../tools/lookupname.pl $(SRCTTF:.ttf=.vtp) > $@
 	rm tmp.sfd
 
 clean:
-	-rm -rf $(SRCTTF) $(SRCDOC) $(SRCTTF:.ttf=.vtp) $(SRCTTF:.ttf=.sfd) *~
+	-rm -rf $(SRCTTF:.ttf=.vtp) $(SRCTTF:.ttf=.sfd) *~
+
+distclean: clean
+	-rm -rf $(SRCTTF) $(BASETTF) $(SRCDOC)
